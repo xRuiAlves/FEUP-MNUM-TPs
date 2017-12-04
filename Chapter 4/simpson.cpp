d2b6a06ed08670e41482a791bf541a1982f7b384
Rui Alves <up201606746@fe.up.pt>
2017-12-04 20:53:36 +0000

    Fixed commenting error in Simpson's method

--- Chapter 4/simpson.cpp
+++ Chapter 4/simpson.cpp
@@ -13,50 +13,50 @@
 
 *******************************************************/
 
-//#include <vector>
-//#include <iostream>
-//
-//
-//double foo ( double x ) { return (x*x*x*x); }
-//
-//int main(){
-//
-//	const double a = 5;
-//	const double b = 10;
-//	const double h = 0.5;
-//	const unsigned int n = (b-a)/h;
-//
-//	double i_odds = 0;
-//	double i_evens = 0;
-//	double integral = 0;
-//
-//	// Make the list
-//	std::vector<double> values;
-//
-//	for (unsigned int i=0 ; i<=n ; i++){
-//		values.push_back(foo(a + i*h));
-//	}
-//
-//	// Compute the odds
-//	for (unsigned int i=1 ; i<n ; i+=2){
-//		i_odds += values.at(i);
-//	}
-//
-//	// Computhe the evens
-//	for (unsigned int i=2 ; i<n ; i+=2){
-//		i_evens += values.at(i);
-//	}
-//
-//	i_odds *= 4;	// Odd terms have coeficient 4
-//	i_evens *= 2;	// Even terms have coeficient 2 (except y0 and yn)
-//	integral += i_odds + i_evens;
-//
-//	integral += values.at(0);	// y0
-//	integral += values.at(n);	// yn
-//
-//	integral *= h/3;	// I = h/3 * (y0 + 4y1 + 2y2 + 4y3 + ... + 4y(n-3) + 2y(n-2) + 4y(n-1) + y(n) )
-//
-//	std::cout << integral;
-//
-//	return 0;
-//}
+#include <vector>
+#include <iostream>
+
+
+double foo ( double x ) { return (x*x*x*x); }
+
+int main(){
+
+   const double a = 5;
+	  const double b = 10;
+	  const double h = 0.5;
+	  const unsigned int n = (b-a)/h;
+
+	  double i_odds = 0;
+	  double i_evens = 0;
+	  double integral = 0;
+
+	  // Make the list
+	  std::vector<double> values;
+
+	  for (unsigned int i=0 ; i<=n ; i++){
+	  	values.push_back(foo(a + i*h));
+  	}
+
+	  // Compute the odds
+	  for (unsigned int i=1 ; i<n ; i+=2){
+	   	i_odds += values.at(i);
+	  }
+
+	  // Computhe the evens
+	  for (unsigned int i=2 ; i<n ; i+=2){
+	   	i_evens += values.at(i);
+	  }
+
+  	i_odds *= 4;	// Odd terms have coeficient 4
+	  i_evens *= 2;	// Even terms have coeficient 2 (except y0 and yn)
+  	integral += i_odds + i_evens;
+
+  	integral += values.at(0);	// y0
+  	integral += values.at(n);	// yn
+
+  	integral *= h/3;	// I = h/3 * (y0 + 4y1 + 2y2 + 4y3 + ... + 4y(n-3) + 2y(n-2) + 4y(n-1) + y(n) )
+
+  	std::cout << integral;
+
+  	return 0;
+}
