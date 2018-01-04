bc3c55f3020e4d25ee6c1ac05914fe84c5b99258
xRuiAlves <up201606746@fe.up.pt>
2018-01-04 14:51:46 +0000

    Made changes in Simpson's and Trapezio's Method C++ Implementations

--- Chapter 4/simpson.cpp
+++ Chapter 4/simpson.cpp
@@ -1,62 +1,103 @@
-/*******************************************************
+/*
+ * simpson.cpp
+ *
+ *  Created on: 04/01/2018
+ *      Author: Rui Alves
+ */
 
- In this example i am integrating x^4 between values
- 5 and 10. With a step equal to 0.5 , i got the value
- 19375 for the integral. The analitacal solution is ...
- ... 19375!
-
- This method works very well, because it doesn't
- have a harder calculation than the "Trapezio" method. Allthough,
- Trapezio is a 2nd order method and Simpson is a 4th order method.
- This means that, with Simpson's method, if we reduce the step (h)
- to half, we are lowering the absolute error 2^order = 2^4 = 16 times!
-
-*******************************************************/
-
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
 
 
-double foo ( double x ) { return (x*x*x*x); }
+double f(double x) { return (5*x*x*x*x + 8.5*x*x*x + 3.14); }	// f(x) = 5x^4 + 8.5x^3 + 3.14
+
+
+/***
+
+  Simpson's method
+
+  a - integral initial value
+  b - integral final value
+  h - step
+
+ ***/
+double simpson(const double a , const double b , const double h){
+
+	const unsigned int n = abs(b-a)/h;
+	double integral = 0;
+	double integral_odd_terms = 0;
+	double integral_even_terms = 0;
+
+	// Make the values list
+	vector<double> values;
+	for (unsigned int i=0 ; i<=n ; i++){
+		values.push_back(f(a + i*h));
+	}
+
+	// Compute the odd values
+	for (unsigned int i=1 ; i<n ; i+=2)	integral_odd_terms += values.at(i);
+
+
+	// Compute the even values
+	for (unsigned int i=2 ; i<n ; i+=2)	integral_even_terms += values.at(i);
+
+	integral_odd_terms *= 4;	// The odd values have coeficient 4
+
+	integral_even_terms *= 2;	// The even values have coeficient 2
+
+	integral += values.at(0);	// y0
+	integral += values.at(n);	// yn
+	integral += integral_odd_terms;
+	integral += integral_even_terms;
+
+	// Multiply by step/3
+
+	integral *= h/3;	// I = h/3 * (y0 + 4y1 + 2y2 + 4y3 + ... + 4y(n-3) + 2y(n-2) + 4y(n-1) + y(n) )
+
+
+	// Return the computed value
+	return integral;
+}
+
+/* Function no compute the Convergence Quotient */
+double quotient(double s , double s_l , double s_ll){  return (s_l - s)/(s_ll - s_l); }
+
+
 
 int main(){
+	// Set printing precision
+	cout << fixed;
+	cout.precision(6);
 
-   const double a = 5;
-	  const double b = 10;
-	  const double h = 0.5;
-	  const unsigned int n = (b-a)/h;
+	// Integral Values
+	const double a = 2;
+	const double b = 6;
+	double h = 0.25;
 
-	  double i_odds = 0;
-	  double i_evens = 0;
-	  double integral = 0;
+	// Compute and print the answers
+	double integral1 = simpson(a , b , h);
+	cout << "Using h = " << h << endl;
+	cout << "Integral: " << integral1 << endl << endl;
 
-	  // Make the list
-	  std::vector<double> values;
+	h /= 2;	// Dimish the step
 
-	  for (unsigned int i=0 ; i<=n ; i++){
-	  	values.push_back(foo(a + i*h));
-  	}
+	double integral2 = simpson(a , b , h);
+	cout << "Using h = " << h << endl;
+	cout << "Integral: " << integral2 << endl << endl;
 
-	  // Compute the odds
-	  for (unsigned int i=1 ; i<n ; i+=2){
-	   	i_odds += values.at(i);
-	  }
+	h /= 2;	// Dimish the step
 
-	  // Computhe the evens
-	  for (unsigned int i=2 ; i<n ; i+=2){
-	   	i_evens += values.at(i);
-	  }
+	double integral3 = simpson(a , b , h);
+	cout << "Using h = " << h << endl;
+	cout << "Integral: " << integral3 << endl << endl;
 
-  	i_odds *= 4;	// Odd terms have coeficient 4
-	  i_evens *= 2;	// Even terms have coeficient 2 (except y0 and yn)
-  	integral += i_odds + i_evens;
+	// Print the convergence quotient
+	double convergence_quotient = quotient(integral1 , integral2 , integral3);
+	cout << "Convergence quotient: " << convergence_quotient << endl;
 
-  	integral += values.at(0);	// y0
-  	integral += values.at(n);	// yn
 
-  	integral *= h/3;	// I = h/3 * (y0 + 4y1 + 2y2 + 4y3 + ... + 4y(n-3) + 2y(n-2) + 4y(n-1) + y(n) )
-
-  	std::cout << integral;
-
-  	return 0;
+	return 0;
 }
--- Chapter 4/trapezio.cpp
+++ Chapter 4/trapezio.cpp
@@ -1,57 +1,94 @@
-/*******************************************************
-
- In this example i am integrating x^2 between values
- 4 and 10. With a step equal to 0.1 , i got the value
- 312.01 for the integral. The analitacal solution is 312.
- The error was very small ( 0.01 ) because i am using a very
- low step!
-
- This method works fine, but Simpson's method works better,
- without harder calculations associated. This method is a 2nd
- order method: When we lower the step by half , the error will
- be reduced by 2^order = 2^2 = 4 times! This is good, but Simpson's
- method, being 4th order, diminishes the error by 16 times by lowering
- the step with the same amount, without more work. That being said,
- Simpson's method is highly prefered!!
-
-*******************************************************/
+/*
+ * trapezio.cpp
+ *
+ *  Created on: 04/01/2018
+ *      Author: Rui Alves
+ */
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-double foo(double x) { return (x*x); }
 
-int main(){
+double f(double x) { return (x*x - 4*x + 3.5); }	// f(x) = x^2 - 4*x + 3.5
 
-	const double a = 4;
-	const double b = 10;
-	const double h = 0.1;
-	const unsigned int n = (b-a)/h;
 
+/***
+
+  Trapezio's method
+
+  a - integral initial value
+  b - integral final value
+  h - step
+
+ ***/
+double trapezio(const double a , const double b , const double h){
+
+	const unsigned int n = abs(b-a)/h;
 	double integral = 0;
 
-	// Make the list
+	// Make the values list
 	vector<double> values;
-	for (double i = 0; i <= n ; i++){
-		values.push_back(foo(a + i*h));
+	for (unsigned int i=0 ; i<=n ; i++){
+		values.push_back(f(a + i*h));
 	}
 
+	// Compute the middle values ( all except y0 and yn )
+	for (unsigned int i=1 ; i<n ; i++)	integral += values.at(i);
 
-	// Compute the middle terms (all except y0 and yn)
-	for (unsigned int i=1 ; i<n ; i++){
-		integral += values[i];
-	}
+	// The middle terms have coeficient 2
+	integral *= 2;
 
-	integral *= 2; 	// The middle terms have coeficient 2
+	integral += values.at(0);	// y0
+	integral += values.at(n);	// yn
 
-	integral += values[0];	// y0
-	integral += values[n];	// yn
+	// Multiply by step/2
 
-	integral *= h/2;		// I = h/2 * (y0 + 2y1 + 2y2 + ... + 2y(n-1) + y(n) )
+	integral *= h/2;	// I = h/2 * (y0 + 2y1 + 2y2 + ... + 2y(n-1) + y(n) )
 
-	cout << integral << endl;
+
+	// Return the computed value
+	return integral;
+}
+
+/* Function no compute the Convergence Quotient */
+double quotient(double s , double s_l , double s_ll){  return (s_l - s)/(s_ll - s_l); }
+
+
+
+int main(){
+	// Set printing precision
+	cout << fixed;
+	cout.precision(6);
+
+	// Integral Values
+	const double a = 2;
+	const double b = 6;
+	double h = 0.25;
+
+	// Compute and print the answers
+	double integral1 = trapezio(a , b , h);
+	cout << "Using h = " << h << endl;
+	cout << "Integral: " << integral1 << endl << endl;
+
+	h /= 2;	// Dimish the step
+
+	double integral2 = trapezio(a , b , h);
+	cout << "Using h = " << h << endl;
+	cout << "Integral: " << integral2 << endl << endl;
+
+	h /= 2;	// Dimish the step
+
+	double integral3 = trapezio(a , b , h);
+	cout << "Using h = " << h << endl;
+	cout << "Integral: " << integral3 << endl << endl;
+
+	// Print the convergence quotient
+	double convergence_quotient = quotient(integral1 , integral2 , integral3);
+	cout << "Convergence quotient: " << convergence_quotient << endl;
+
 
 	return 0;
 }
